d3cbd64a7c8795d1ca4042343c5dc32d790f817b
211 <211@localhost.localdomain>
2019-10-15 15:57:06 +0900

    예제 5-3) 수정

--- EchoServer.c
+++ EchoServer.c
@@ -51,44 +51,44 @@ int main(){
 		printf("클라이언트 접속 허용\n");
 
 		while(1) {
-		n = read(c_socket, rcvBuffer, sizeof(rcvBuffer));
-		rcvBuffer[n] = '\0';
-		printf("메시지 받음 : %s\n", rcvBuffer);
-		
-		if(strncasecmp(rcvBuffer, "quit", 4) == 0 || strncasecmp(rcvBuffer, "kill server", 11) == 0)
-				break;
-		else if(!strncmp(rcvBuffer, "안녕하세요", strlen("안녕하세요")))
-			strcpy(buffer, "안녕하세요. 만나서 반가워요.");
-		else if(!strncmp(rcvBuffer, "이름이 머야?", strlen("이름이 머야?")))
-				strcpy(buffer, "내 이름은 강혜경이야.");
-		else if(!strncmp(rcvBuffer, "몇 살이야?", strlen("몇 살이야?")))
-				strcpy(buffer, "나는 22살이야.");
-		else if(!strncasecmp(rcvBuffer, "strlen ", 7))
-				sprintf(buffer, "문자열의 길이 : %d", strlen(rcvBuffer)-7);
-		else if(!strncasecmp(rcvBuffer, "strcmp ", 7)){
-				i = 0;
-				token = strtok(rcvBuffer, " ");
-			while(token != NULL){
-					str[i++] = token;
-					token = strtok(NULL, " ");
-			}
-			if(i<3)
-				sprintf(buffer, "문자열 비교를 위해서는 두 문자열이 필요합니다.");
-			else if(!strcmp(str[1], str[2]))
-				sprintf(buffer, "%s %s : 0 입니다.", str[1], str[2]);
+			n = read(c_socket, rcvBuffer, sizeof(rcvBuffer));
+			printf("메시지 받음 : %s\n", rcvBuffer);
+			rcvBuffer[n] = '\0'; //개행 문자 삭제
+
+			if(strncasecmp(rcvBuffer, "quit", 4) == 0 || strncasecmp(rcvBuffer, "kill server", 11) == 0)
+					break;
+			else if(!strncmp(rcvBuffer, "안녕하세요", strlen("안녕하세요")))
+				strcpy(buffer, "안녕하세요. 만나서 반가워요.");
+			else if(!strncmp(rcvBuffer, "이름이 머야?", strlen("이름이 머야?")))
+					strcpy(buffer, "내 이름은 강혜경이야.");
+			else if(!strncmp(rcvBuffer, "몇 살이야?", strlen("몇 살이야?")))
+					strcpy(buffer, "나는 22살이야.");
+			else if(!strncasecmp(rcvBuffer, "strlen ", 7))
+					sprintf(buffer, "문자열의 길이 : %d", strlen(rcvBuffer)-7);
+			else if(!strncasecmp(rcvBuffer, "strcmp ", 7)){
+					i = 0;
+					token = strtok(rcvBuffer, " ");
+				while(token != NULL){
+						str[i++] = token;
+						token = strtok(NULL, " ");
+				}
+				if(i<3)
+					sprintf(buffer, "문자열 비교를 위해서는 두 문자열이 필요합니다.");
+				else if(!strcmp(str[1], str[2]))
+					sprintf(buffer, "%s %s : 같은 문자열 입니다.", str[1], str[2]);
+				else
+					sprintf(buffer, "%s %s : 다른 문자열 입니다.", str[1], str[2]);
+				}
 			else
-				sprintf(buffer, "%s %s : 1입니다.", str[1], str[2]);
-			}
-		else
-			strcpy(buffer, "무슨 말인지 모르겠습니다.");
+				strcpy(buffer, "무슨 말인지 모르겠습니다.");
 		
-		n = strlen(buffer);
-		write(c_socket, buffer, n);
-		}
-		close(c_socket);
-		if (strncasecmp(rcvBuffer, "kill server", 11) == 0)
-			break;
-		}
+			n = strlen(buffer);
+			write(c_socket, buffer, n);
+			}
+			close(c_socket);
+			if (strncasecmp(rcvBuffer, "kill server", 11) == 0)
+				break;
+			}
 	close(s_socket);
 	return 0;	
 }
