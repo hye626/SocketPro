b3496ae513647e7e0c281052b988970a7b0ea344
211 <211@localhost.localdomain>
2019-12-03 15:07:00 +0900

    채팅 프로그램 구현

--- MultiThreadCharClient.c
+++ MultiThreadCharClient.c
@@ -14,71 +14,74 @@
 #define WHISPER "/w"
 #define DELIMETER " "
 
-void *do_send_chat(void *); //두개의 스레드 생성
+void *do_send_chat(void *);
 void *do_receive_chat(void *);
-pthread_t thread_1, thread_2;
+pthread_t thread_1, thread_2; //두 개의 스레드 생성
 
 char    escape[ ] = "exit";
 char    nickname[20];
 
 int main(int argc, char *argv[ ])
 {
-    int c_socket;
-    struct sockaddr_in c_addr;
+    int c_socket; //소켓을 담을 변수 선언
+    struct sockaddr_in c_addr; //소켓 구조체
     int len;
     char chatData[CHATDATA];
     char buf[CHATDATA];
     int nfds;
-    fd_set read_fds;
+    fd_set read_fds; //fd 구조체
     int n;
-    c_socket = socket(PF_INET, SOCK_STREAM, 0);
-    memset(&c_addr, 0, sizeof(c_addr));
+    c_socket = socket(PF_INET, SOCK_STREAM, 0); //소켓 생성
+    memset(&c_addr, 0, sizeof(c_addr)); //소켓 구조체 설정
     c_addr.sin_addr.s_addr = inet_addr(IPADDR);
     c_addr.sin_family = AF_INET;
     c_addr.sin_port = htons(PORT);
     printf("Input Nickname : ");
-    scanf("%s", nickname);
+    scanf("%s", nickname); //닉네임 설정
 
-    if(connect(c_socket, (struct sockaddr *) &c_addr, sizeof(c_addr)) == -1) {
+    if(connect(c_socket, (struct sockaddr *) &c_addr, sizeof(c_addr)) == -1) { //소켓 연결
         printf("Can not connect\n");
         return -1;
     }
-	write(c_socket, nickname, strlen(nickname));	
-	pthread_create(&thread_1, NULL, do_send_chat, (void *) &c_socket);
+	write(c_socket, nickname, strlen(nickname));
+	pthread_create(&thread_1, NULL, do_send_chat, (void *) &c_socket); //스레드 생성
 	pthread_create(&thread_2, NULL, do_receive_chat, (void *) &c_socket);
-	pthread_join(thread_1, NULL);
+	// 1: 성공적으로 함수가 호출되면 이곳에 스레드 ID 저장 이 인자로 pthread_join 함수 사용
+	// 2: 스레드의 특성 정의 기본적으로 NULL 3: 어떤 로직을 할지 함수포인터를 매개변수로 받음 4:전달될 인자
+	pthread_join(thread_1, NULL); //th로 시작된 스레드가 종료되는걸 기다림
 	pthread_join(thread_2, NULL);
 	close(c_socket);
 }
 
-void * do_send_chat(void *arg)
+void * do_send_chat(void *arg) //채팅 보내는 함수
 {
     char chatData[CHATDATA];
     char buf[CHATDATA];
     int n;
-    int c_socket = *((int *) arg);        // client socket
+    int c_socket = *((int *) arg);  //클라이언트 소켓
     while(1) {
-        memset(buf, 0, sizeof(buf));
-        if((n = read(0, buf, sizeof(buf))) > 0 ) {
-			char *token = NULL;
+        memset(buf, 0, sizeof(buf)); //어떤 메모리의 시작점부터 연속된 범위를 모두 지정
+        if((n = read(0, buf, sizeof(buf))) > 0 ) { //버퍼의 사이즈만큼 읽기 0은 파일의 끝을 나타내기 때문에 0보다 크다면
+			char *token = NULL; //채팅에 사용할 변수 선언
 			char *toNickname = NULL;
 			char *message = NULL;
-			if(strncasecmp(buf, WHISPER, 2) == 0){
-				token = strtok(buf, DELIMETER);
-				if(token != NULL)
+			if(strncasecmp(buf, WHISPER, 2) == 0){ //입력한 버퍼가 /w 와 같다면
+				token = strtok(buf, DELIMETER); //DELIMETER만큼 문자열 잘라서 token에  /w 저장
+				if(token != NULL)  //토큰이 NULL값이 아니면 문자열을 DELIMETER 단위로 분리해서 닉네임 저장
 					toNickname = strtok(NULL, DELIMETER);
-				if(toNickname != NULL)
+				if(toNickname != NULL) //나머지 메시지 잘라서 저장
 					message = strtok(NULL, "\0");
 				if(token == NULL || toNickname == NULL || message ==  NULL){
 					printf("Your whisper message is wrong. Please input '/w nickname message'\n");
-					continue;
+					continue; //셋 중 하나라도 NULL이면 오류메시지 출력하여 귓속말 전송 실패
 				}
 
 				sprintf(chatData, "%s %s [%s] %s", token, toNickname, nickname, message);
+				// 귓속말 문자열을 만들어서 charData에 문자열의 길이 저장
 			} else { 
-				sprintf(chatData, "[%s] %s", nickname, buf);
+				sprintf(chatData, "[%s] %s", nickname, buf); //일반 채팅 메시지 문자열을 만들어서 chatData에 저장
 			}
-			write(c_socket, chatData, strlen(chatData)); //서버로 채팅 메시지 전달
+			write(c_socket, chatData, strlen(chatData)); // 채팅 메시지 전달
 			if(!strncmp(buf, escape, strlen(escape))) { //'exit' 메세지를 입력하면
 				pthread_kill(thread_2, SIGINT); //do_receive_char 스레드를 종료시킴
 				break; //자신도 종료
@@ -88,12 +91,12 @@ void * do_send_chat(void *arg)
 }
 void *do_receive_chat(void *arg)
 {
-    char   chatData[CHATDATA];
+    char   chatData[CHATDATA]; //메시지 변수
     int    n;
-    int    c_socket = *((int *)arg);        // client socket
+    int    c_socket = *((int *)arg); //클라이언트 소켓
     while(1) {
         memset(chatData, 0, sizeof(chatData));
-        if((n = read(c_socket, chatData, sizeof(chatData))) > 0 ) {
+        if((n = read(c_socket, chatData, sizeof(chatData))) > 0 ) { //
             write(1, chatData, n); //chatData를 화면에 출력함 (1 = stdout (모니터))
         }
     }
--- MultiThreadCharServer.c
+++ MultiThreadCharServer.c
@@ -20,7 +20,7 @@ pthread_mutex_t mutex;
 #define WHISPER "/w"
 #define DELIMETER " "
 
-struct chatClient {
+struct chatClient { //클라이언트 구조체
 	int c_socket;
 	char nickname[CHATDATA];
 };
@@ -40,18 +40,18 @@ int main(int argc, char *argv[ ])
     int    res;
 	 char nickname[CHATDATA];
 
-    if(pthread_mutex_init(&mutex, NULL) != 0) { //mutex 초기화
-        printf("Can not create mutex\n");
+    if(pthread_mutex_init(&mutex, NULL) != 0) { //mutex 초기화 두번째 인자로 기본 뮤텍스 특징을 사용하려면  NULL
+        printf("Can not create mutex\n"); //실패했을 경우 반환값 -1
         return -1;
     }
     s_socket = socket(PF_INET, SOCK_STREAM, 0); // 서버소켓 생성
-
-    memset(&s_addr, 0, sizeof(s_addr));
+ 
+    memset(&s_addr, 0, sizeof(s_addr)); //서버 소켓 초기화
     s_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     s_addr.sin_family = AF_INET;
     s_addr.sin_port = htons(PORT);
 
-    if(bind(s_socket, (struct sockaddr *)&s_addr, sizeof(s_addr)) == -1) {
+    if(bind(s_socket, (struct sockaddr *)&s_addr, sizeof(s_addr)) == -1) { //서버 소켓 바인드
         printf("Can not Bind\n");
         return -1;
     }
@@ -61,14 +61,14 @@ int main(int argc, char *argv[ ])
         return -1;
     }
 
-    for(i = 0; i < MAX_CLIENT; i++) //리스트 클라이언트 목록을 초기화
+    for(i = 0; i < MAX_CLIENT; i++) //리스트 클라이언트 목록을 -1로 초기화
         list_c[i].c_socket = INVALID_SOCK;
 
     while(1) { //accept 대기
         len = sizeof(c_addr);
-        c_socket = accept(s_socket, (struct sockaddr *) &c_addr, &len);
+        c_socket = accept(s_socket, (struct sockaddr *) &c_addr, &len); //클라이언트와 연결할 소켓 생성
 
-		if((n = read(c_socket, nickname, sizeof(nickname))) < 0){
+		if((n = read(c_socket, nickname, sizeof(nickname))) < 0){ //닉네임 읽기
 			printf("nickname read fail.\n");
 			return -1;
 		}
@@ -79,9 +79,9 @@ int main(int argc, char *argv[ ])
             write(c_socket, CODE200, strlen(CODE200));
             close(c_socket);
         } else {
-			write(c_socket, greeting, strlen(greeting));
-			pthread_create(&thread, NULL, do_chat, (void*)&c_socket);
-           //pthread_create with do_chat function.
+			write(c_socket, greeting, strlen(greeting)); //환영메시지 출력
+			pthread_create(&thread, NULL, do_chat, (void*)&c_socket); //스레드 생성
+           //**********pthread_create with do_chat function****************
 			//스레드 생성 인자값 : 1) 생성된 스레드의 식별자를 저장할 변수의 포인터, 2) 일반적으로 NULL, 3)void *인 함수를 가리키는 포인터
 			//4)스레드에 의해 호출되는 함수에 전달하고자 하는 인자
         	}
@@ -91,31 +91,34 @@ int main(int argc, char *argv[ ])
 void *do_chat(void *arg) //현재 접속한 해당 클라이언트가 메시지 보내는 걸 읽음
 {
     int c_socket = *((int *)arg);
-    char chatData[CHATDATA];
+    char chatData[CHATDATA]; //메시지 배열
     int i, n;
     while(1) {
         memset(chatData, 0, sizeof(chatData)); //챗데이터에 저장
-        if((n = read(c_socket, chatData, sizeof(chatData))) > 0){
+        if((n = read(c_socket, chatData, sizeof(chatData))) > 0){ // 읽기 성공하면
+			//***************************************************************************
 		
 			char *token = NULL;
 			char *nickname = NULL;
 			char *message = NULL;
 
-			if(strncasecmp(chatData, WHISPER, 2) == 0){
-				token = strtok(chatData, DELIMETER); // \w
-				nickname = strtok(NULL, DELIMETER); //nickname
-				message = strtok(NULL, "\0"); 
+			if(strncasecmp(chatData, WHISPER, 2) == 0){ //입력한 채팅과 WHISPER가 같으면
+				token = strtok(chatData, DELIMETER); // 문자열을 DELIMETER까지 분리
+				nickname = strtok(NULL, DELIMETER); //다음 구분자로 연속되는 토큰인 nickname 저장
+				message = strtok(NULL, "\0"); //메시지 저장
 			}
-			for(i = 0; i < MAX_CLIENT; i++) {
+
+			for(i = 0; i < MAX_CLIENT; i++) { //클라이언트 최대 연결까지
 				if(list_c[i].c_socket != INVALID_SOCK) {
 					if(nickname != NULL){
-						if(strcasecmp(list_c[i].nickname, nickname) == 0)
-							write(list_c[i].c_socket, message, strlen(message));
+						if(strcasecmp(list_c[i].nickname, nickname) == 0) //연결된 클라이언트와 닉네임이 같으면
+							write(list_c[i].c_socket, message, strlen(message)); //귓속말 쓰기
 					}else{
-						write(list_c[i].c_socket, chatData, n);
+						write(list_c[i].c_socket, chatData, n); //아니면 일반 채팅쓰기
 					}
 				}
 			}
+			//************************************************************************
 			if(strstr(chatData, "/exit") != NULL) { //클라이언트가 보낸 메시지가 exit이면 대화방 나가기
                 popClient(c_socket); //클라이언트 목록에서 삭제
                 break;
@@ -124,15 +127,15 @@ void *do_chat(void *arg) //현재 접속한 해당 클라이언트가 메시지 
     	}
 }
 
-int pushClient(int c_socket, char *nickname) {
+int pushClient(int c_socket, char *nickname) { //새로운 연결소켓을 list_c에 추가하는 함수
 	int i;
 	for(i = 0; i < MAX_CLIENT; i++) {
-		if(list_c[i].c_socket == INVALID_SOCK) {
-			pthread_mutex_lock(&mutex);
+		if(list_c[i].c_socket == INVALID_SOCK) { //list를 -1로 초기화한게 맞으면
+			pthread_mutex_lock(&mutex); //락 매커니즘을 사용하여 동시에 하나 이상의 스레드가 공유 자원에 접근하는 것을 방지
 			list_c[i].c_socket = c_socket;
-			memset(list_c[i].nickname, 0, CHATDATA);
+			memset(list_c[i].nickname, 0, CHATDATA);//구조체 크기만큼 0으로 초기화
 			strcpy(list_c[i].nickname, nickname);
-			pthread_mutex_unlock(&mutex);
+			pthread_mutex_unlock(&mutex); //공유자원의 사용을 마치면 락을 풀어줌
 			return i;
 		}
 	}
@@ -140,13 +143,13 @@ int pushClient(int c_socket, char *nickname) {
 		return -1;
 }	
 
-int popClient(int c_socket)
-{			
-	int i;	 
-	close(c_socket); 
-	for(i = 0; i < MAX_CLIENT; i++) {
+int popClient(int c_socket) //기존채팅 클라이언트의 탈퇴를 처리하는 함수
+	{			
+	int i; //인자로 받은 연결소켓을 배열 list_c에서 제거하기 위함
+	close(c_socket); //소켓 종료
+	for(i = 0; i < MAX_CLIENT; i++) { //모든 클라이언트에 대하여
 		if(c_socket == list_c[i].c_socket) {
-			pthread_mutex_lock(&mutex);
+			pthread_mutex_lock(&mutex); //스레드 락을 건 후, 다시 -1로 초기화 함
 			list_c[i].c_socket = INVALID_SOCK;
 			pthread_mutex_unlock(&mutex);
 			break;
